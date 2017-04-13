cc2a7e4d2231db3a5dd899560200ce19a94199ff
Nicolás Huertas Gnecco <nicolashuertasgnecco@gmail.com>
2017-04-13 18:31:43 -0500

    programing 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The function will reduce the time it takes to calculate an invertibale matrix 
+## it will baasiclly search if the matrix has been already inverted if it does it 
+##will return the inverted matrix 
 
-## Write a short comment describing this function
+## the function makes the list to get the original matrix set de matrix 
+## get the inverse or set de inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        inversa <- NULL
+        set <- function(y) {
+                x <<- y
+                inversa <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(inverse) inversa <<- inverse
+        getinverse <- function() inversa
+        list(set = set,
+                get = get,
+                setinverse = setinverse,
+                getinverse = getinverse)
+        
 }
 
 
-## Write a short comment describing this function
+##The function checks if there has already been a calculation of the inverse and
+## if there has been it returns the inverse if not it calculates it
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ...) 
+{
+     
+        inversa <- x$getinverse()
+        if (!is.null(inversa)) {
+                message("getting cached data")
+                return(inversa)
+        }
+        print("recalculating")
+        data <- x$get()
+        inversa <- solve(data, ...)
+        x$setinverse(inversa)
+        inversa
+        
 }
